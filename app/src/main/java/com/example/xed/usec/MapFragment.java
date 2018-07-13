6ec1b6935355f22aafdd4a0be934ba6ee192ff1a
Allan Zed <mubangizia22@gmail.com>
2018-07-13 07:38:05 +0300

    introduced map fragment

--- app/src/main/java/com/example/xed/usec/MapFragment.java
+++ app/src/main/java/com/example/xed/usec/MapFragment.java
@@ -7,11 +7,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap mgoogleMap;
+    MapView mapView;
+    View mview;
 
 
     public MapFragment() {
@@ -23,7 +34,33 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        mview = inflater.inflate(R.layout.fragment_map, container, false);
+
+        return mview;
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view,savedInstanceState);
+        mapView = (MapView) mview.findViewById(R.id.map_view);
+
+        if(mapView!=null){
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+
+        }
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mgoogleMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng kampala = new LatLng(0.341665, 32.561484);
+        mgoogleMap.addMarker(new MarkerOptions().position(kampala).title("Marker in Kampala"));
+        mgoogleMap.moveCamera(CameraUpdateFactory.newLatLng(kampala));
+    }
 }
